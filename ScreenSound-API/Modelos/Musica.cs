2ceb263033bf5f3b4119dbe23f71a98cfe73f532
Jonathan Hora <jonathan.s.hora@gmail.com>
2024-04-03 10:39:42 -0300

    deserializando dados

--- ScreenSound-API/Modelos/Musica.cs
+++ ScreenSound-API/Modelos/Musica.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ScreenSound_API.Modelos
 {
     internal class Musica
     {
+        [JsonPropertyName("song")]
+        public string? Nome { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public int Duracao { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string? Genero { get; set; }
+
+        public void ExibirDetalhesDaMusica()
+        {
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Música: {Nome}");
+            Console.WriteLine($"Duração: {Duracao/1000}");
+            Console.WriteLine($"Gêreno: {Genero}");
+        }
     }
 }
--- ScreenSound-API/Program.cs
+++ ScreenSound-API/Program.cs
@@ -1,9 +1,13 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSound_API.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
